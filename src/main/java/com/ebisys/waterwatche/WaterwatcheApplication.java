574fa870da8b2cae5c560accbc7c34d61320848d
abdessamadalami <eloualialamiabdessamad@gmail.com>
2023-11-14 17:23:43 +0100

    add file pox

--- src/main/java/com/ebisys/waterwatche/WaterwatcheApplication.java
+++ src/main/java/com/ebisys/waterwatche/WaterwatcheApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WaterwatcheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WaterwatcheApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(WaterwatcheApplication.class, args);
+                System.out.println(" W salamo ");
+
+
+    }
 }
--- src/test/java/com/ebisys/waterwatche/WaterwatcheApplicationTests.java
+++ src/test/java/com/ebisys/waterwatche/WaterwatcheApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WaterwatcheApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
